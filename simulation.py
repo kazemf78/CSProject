96de5f58fbe7a46f1fc207d64745a981d05d5706
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2019-06-10 22:45:02 +0430

    1 2 3 statistics added

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 
 
 def get_exp_sample(rate):
@@ -61,13 +62,19 @@ def arrive(event):
             timer_queue[0].append([event[0] + dead_line, event[0]])
 
 
-def execute(arrive_time, now, server_idx):
+def execute(kind, arrive_time, now, server_idx):
     # it's guaranteed that there is an event to be added to a core of this server
+    if exited > 5000:
+        exit_from_queue_num[kind] += 1
+        sum_queue_wait[kind] += now - arrive_time
+        square_queue_wait[kind] += (now - arrive_time) ** 2
+        check_statistics()
     for i in range(len(cores[server_idx])):
         if not core_is_busy[server_idx][i]:
             serve_time = get_exp_sample(cores[server_idx][i])
             core_task_arrive_time[server_idx][i] = arrive_time
             add_event([now + serve_time, [server_idx, i]])
+            core_task_class[server_idx][i] = kind
             return
 
 
@@ -84,32 +91,115 @@ def timer_pass(event):
             tmp_idxs.append(i)
     rand_num = np.random.randint(0, len(tmp_idxs))
     idx = tmp_idxs[rand_num]
-    if server_queue_length(cores_queue[idx]) == 0:
-        for is_busy in core_is_busy[idx]:
-            if not is_busy:
-                execute(event[0], idx)
-                return
     if len(timer_queue[0]) == 0 and len(timer_queue[1]) == 0:
         return
     if len(timer_queue[0]):
         res = timer_queue[0].pop(0)
-        cores_queue[idx][0].append(res)
+        kind = 0
     else:
         res = timer_queue[1].pop(0)
+        kind = 1
+    for is_busy in core_is_busy[idx]:
+        if not is_busy:
+            execute(kind, res[1], event[0], idx)
+            return
+    if kind == 0:
+        cores_queue[idx][0].append(res)
+    else:
         cores_queue[idx][1].append(res)
 
 
+def check_accuracy(n, sum_squares, sum):
+    if sum == 0 or n == 0:
+        return False
+    mean = sum / float(n)
+    sigma = sqrt(sum_squares / float(n))
+    return (1.96 * sigma) / (sqrt(float(n)) * mean) <= 0.05
+
+
+def check_statistics():
+    if check_accuracy(sum(exited_users), sum(square_exited_users), sum(sum_exited_users)) and is_done[0][0] == 0:
+        is_done[0][0] = sum(exited_users)
+        output[0][0] = sum(sum_exited_users) / sum(exited_users)
+    if check_accuracy(exited_users[0], square_exited_users[0], sum_exited_users[0]) and is_done[0][1] == 0:
+        is_done[0][1] = exited_users[0]
+        output[0][1] = sum_exited_users[0] / exited_users[0]
+        # print(sum_exited_users[0])
+    if check_accuracy(exited_users[1], square_exited_users[1], sum_exited_users[1]) and is_done[0][2] == 0:
+        is_done[0][2] = exited_users[1]
+        output[0][2] = sum_exited_users[1] / exited_users[1]
+
+    if check_accuracy(sum(exit_from_queue_num), sum(square_queue_wait), sum(sum_queue_wait)) and is_done[1][0] == 0:
+        is_done[1][0] = sum(exit_from_queue_num)
+        output[1][0] = sum(sum_queue_wait) / sum(exit_from_queue_num)
+    if check_accuracy(exit_from_queue_num[0], square_queue_wait[0], sum_queue_wait[0]) and is_done[1][1] == 0:
+        is_done[1][1] = exit_from_queue_num[0]
+        output[1][1] = sum_queue_wait[0] / exit_from_queue_num[0]
+        print("type 0 " + str(is_done[1][1]))
+    if check_accuracy(exit_from_queue_num[1], square_queue_wait[1], sum_queue_wait[1]) and is_done[1][2] == 0:
+        is_done[1][2] = exit_from_queue_num[1]
+        output[1][2] = sum_queue_wait[1] / exit_from_queue_num[1]
+        print("type 1 " + str(is_done[1][2]))
+
+    if check_accuracy(num_of_expired_tasks[2], square_average_expired_task[2], sum_average_expired_tasks[2]) and \
+            is_done[2][0] == 0:
+        is_done[2][0] = num_of_expired_tasks[2]
+        output[2][0] = sum_average_expired_tasks[2] / num_of_expired_tasks[2]
+
+    if check_accuracy(num_of_expired_tasks[0], square_average_expired_task[0], sum_average_expired_tasks[0]) and \
+            is_done[2][1] == 0:
+        is_done[2][1] = num_of_expired_tasks[0]
+        output[2][1] = sum_average_expired_tasks[0] / num_of_expired_tasks[0]
+
+    if check_accuracy(num_of_expired_tasks[1], square_average_expired_task[1], sum_average_expired_tasks[1]) and \
+            is_done[2][2] == 0:
+        is_done[2][2] = num_of_expired_tasks[1]
+        output[2][2] = sum_average_expired_tasks[1] / num_of_expired_tasks[1]
+
+    # if check_accuracy(sum(num_of_expired_tasks), sum(square_average_expired_task), sum(sum_expired_tasks)) and \
+    #         is_done[1][0] == 0:
+    #     is_done[1][0] = sum(num_of_expired_tasks)
+    #     output[1][0] = sum(sum_expired_tasks) / sum(num_of_expired_tasks)
+    #
+    #
+    # if check_accuracy(num_of_expired_tasks[0], square_average_expired_task[0], sum_expired_tasks[0]) and is_done[1][
+    #     1] == 0:
+    #     is_done[1][1] = num_of_expired_tasks[0]
+    #     output[1][1] = sum_expired_tasks[0] / num_of_expired_tasks[0]
+    #
+    # if check_accuracy(num_of_expired_tasks[1], square_average_expired_task[1], sum_expired_tasks[1]) and is_done[1][
+    #     2] == 0:
+    #     is_done[1][2] = num_of_expired_tasks[1]
+    #     output[1][2] = sum_expired_tasks[1] / num_of_expired_tasks[1]
+
+
 def core_clock(event):
+    global exited_users
+    global sum_exited_users
+    global square_exited_users
+    global exited
     core_is_busy[event[1][0]][event[1][1]] = False
+    kind = core_task_class[event[1][0]][event[1][1]]
+    if exited > 5000:
+        exited_users[kind] += 1
+        new = event[0] - core_task_arrive_time[event[1][0]][event[1][1]]
+        sum_exited_users[kind] += new
+        square_exited_users[kind] += new * new
+        check_statistics()
+    # sum_expired_tasks[kind] += event[0] - core_task_arrive_time[event[1][0]][event[1][1]]
+    # sum_average_expired_tasks += su
     if len(cores_queue[event[1][0]][0]) > 0:
+        execute(0, core_task_arrive_time[event[1][0]][event[1][1]], event[0], event[1][0])
         cores_queue[event[1][0]][0].pop(0)
-        execute(event[0], event[1][0])
     elif len(cores_queue[event[1][0]][1]) > 0:
         cores_queue[event[1][0]][1].pop(0)
-        execute(event[0], event[1][0])
+        execute(1, core_task_arrive_time[event[1][0]][event[1][1]], event[0], event[1][0])
+    exited += 1
 
 
 def handle_expired_tasks(time):
+    global exited_users
+    global exited
     expired_tasks = []
     for i in range(2):
         j = 0
@@ -129,18 +219,58 @@ def handle_expired_tasks(time):
     server_idxs = [[[0, 0]] * M]
     # print(server_idxs)
     for i in range(len(expired_tasks)):
+        if exited > 5000:
+            num_of_expired_tasks[2] += 1
+            num_of_expired_tasks[expired_tasks[i][1]] += 1
+            exited_users[expired_tasks[i][1]] += 1
+            exit_from_queue_num[expired_tasks[i][1]] += 1
+            sum_average_expired_tasks[2] += (num_of_expired_tasks[2]) / sum(exited_users)
+            sum_average_expired_tasks[expired_tasks[i][1]] += (num_of_expired_tasks[expired_tasks[i][1]]) / \
+                                                              exited_users[expired_tasks[i][1]]
+            square_average_expired_task[2] += ((num_of_expired_tasks[2]) / sum(exited_users)) ** 2
+            square_average_expired_task[expired_tasks[i][1]] = ((num_of_expired_tasks[expired_tasks[i][1]]) /
+                                                                exited_users[expired_tasks[i][1]]) ** 2
+
         if expired_tasks[i][0] == -1:
+            # print(expired_tasks)
+            # print(i)
+            # print(timer_idx)
+            # print(expired_tasks)
+            # print(timer_queue)
+            # print(expired_tasks[i][2] - timer_idx[expired_tasks[i][1]])
+            # print()
+            if exited > 5000:
+                sum_queue_wait[expired_tasks[i][1]] += time - timer_queue[expired_tasks[i][1]][
+                    expired_tasks[i][2] - timer_idx[expired_tasks[i][1]]][1]
+                square_queue_wait[expired_tasks[i][1]] += (time - timer_queue[expired_tasks[i][1]][
+                    expired_tasks[i][2] - timer_idx[expired_tasks[i][1]]][1]) ** 2
+                sum_exited_users[expired_tasks[i][1]] += time - timer_queue[expired_tasks[i][1]][
+                    expired_tasks[i][2] - timer_idx[expired_tasks[i][1]]][1]
+                square_exited_users[expired_tasks[i][1]] += (time - timer_queue[expired_tasks[i][1]][
+                    expired_tasks[i][2] - timer_idx[expired_tasks[i][1]]][1]) ** 2
             timer_queue[expired_tasks[i][1]].pop(expired_tasks[i][2] - timer_idx[expired_tasks[i][1]])
             timer_idx[expired_tasks[i][1]] += 1
         else:
-            cores_queue[expired_tasks[i][0]][expired_tasks[i][1]].remove(
+            if exited > 5000:
+                sum_queue_wait[expired_tasks[i][1]] += time - cores_queue[
+                    expired_tasks[i][2] - server_idxs[expired_tasks[i][0]][expired_tasks[i][1]]]
+                square_queue_wait[expired_tasks[i][1]] += (time - cores_queue[
+                    expired_tasks[i][2] - server_idxs[expired_tasks[i][0]][expired_tasks[i][1]]]) ** 2
+                sum_exited_users[expired_tasks[i][1]] += time - cores_queue[
+                    expired_tasks[i][2] - server_idxs[expired_tasks[i][0]][expired_tasks[i][1]]]
+                square_exited_users[expired_tasks[i][1]] += (time - cores_queue[
+                    expired_tasks[i][2] - server_idxs[expired_tasks[i][0]][expired_tasks[i][1]]]) ** 2
+            cores_queue[expired_tasks[i][0]][expired_tasks[i][1]].pop(
                 expired_tasks[i][2] - server_idxs[expired_tasks[i][0]][expired_tasks[i][1]])
             server_idxs[expired_tasks[i][0]][expired_tasks[i][1]] += 1
+        if exited > 5000:
+            check_statistics()
+
+        exited += 1
 
 
 if __name__ == '__main__':
     file = open('config.txt', 'r')
-
     M, arrival_rate, alpha, mu = file.readline()[:-1].split(" ")
     M = int(M)
     arrival_rate = float(arrival_rate)
@@ -153,31 +283,42 @@ if __name__ == '__main__':
     cores = []
     core_is_busy = []
     core_task_arrive_time = []
+    core_task_class = []
 
     # quality properties
-    service_time = []
+    exited = 0
+    sum_exited_users = [0, 0]
     queue_time = []
-    num_of_expired_tasks = []
+    num_of_expired_tasks = [0, 0, 0]
+    square_average_expired_task = [0, 0, 0]
+    sum_average_expired_tasks = [0, 0, 0]
+    # sum_expired_tasks = [0, 0]
     average_timer_queue_len = 0
     average_servers_queue_len = [0] * M
     task_needed_service_time = 0
     task_needed_queue_time = 0
-    task_needed_expired_average = 0
     task_needed_average_timer_queue_len = 0
     task_needed_average_servers_queue_len = [0] * M
     served_users = 0
     intered_users = 0
+    exited_users = [0, 0]
+    square_exited_users = [0, 0]
+    sum_queue_wait = [0, 0]
+    square_queue_wait = [0, 0]
+    exit_from_queue_num = [0, 0]
     limit = 50000000
 
     for i in range(M):
         cores.append([])
         core_is_busy.append([])
         core_task_arrive_time.append([])
+        core_task_class.append([])
         cores_queue.append([[], []])
         temp = file.readline()[:-1].split(" ")
         temp2 = temp[1:]
         core_is_busy[i] = [False] * int(temp[0])
-        core_task_arrive_time.append = [-1] * int(temp[0])
+        core_task_arrive_time[i] = [-1] * int(temp[0])
+        core_task_class[i] = [-1] * int(temp[0])
         for j in range(int(temp[0])):
             cores[i].append(float(temp2[j]))
 
@@ -189,7 +330,11 @@ if __name__ == '__main__':
     for i in range(len(events) - 1, 0, -1):
         BD(i)
 
+    output = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    is_done = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
     while served_users < 50000000:
+        # print(events)
         e = remove_min()
         handle_expired_tasks(e[0])
         if e[1][0] == -2:
